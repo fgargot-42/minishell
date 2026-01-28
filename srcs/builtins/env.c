543f09bed0592e8a2c565f0e5ca5ce3c3fa5c23b
Florent Gargot <fgargot@3C2.42angouleme.fr>
2026-01-28 17:44:02 +0100

    Added implementation of built-in env

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,14 +6,23 @@
 /*   By: mabarrer <mabarrer@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 21:23:55 by mabarrer          #+#    #+#             */
-/*   Updated: 2026/01/27 21:32:54 by mabarrer         ###   ########.fr       */
+/*   Updated: 2026/01/28 17:41:40 by fgargot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 int	builtin_env(t_cmd *cmd)
 {
-	(void)cmd;
+	t_env	current;
+
 	printf("<ENV>\n");
+	if (!cmd && !cmd->env)
+		return (1);
+	current = cmd->env;
+	while (current && current->key)
+	{
+		printf("%s=%s\n", current->key, current->value);
+		current = current->next;
+	}
 	return (0);
 }
