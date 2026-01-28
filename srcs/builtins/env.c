42b255d5887913217281c087fcc12fa2d9af079f
Florent Gargot <fgargot@3C2.42angouleme.fr>
2026-01-28 18:57:50 +0100

    Edited built-in env function

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,22 +6,23 @@
 /*   By: mabarrer <mabarrer@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 21:23:55 by mabarrer          #+#    #+#             */
-/*   Updated: 2026/01/28 17:41:40 by fgargot          ###   ########.fr       */
+/*   Updated: 2026/01/28 18:56:26 by fgargot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-int	builtin_env(t_cmd *cmd)
+int	builtin_env(t_cmd *cmd, t_env **envs)
 {
 	t_env	current;
 
 	printf("<ENV>\n");
-	if (!cmd && !cmd->env)
+	if (!cmd && !envs)
 		return (1);
-	current = cmd->env;
+	current = *envs;
 	while (current && current->key)
 	{
-		printf("%s=%s\n", current->key, current->value);
+		if (current->value && current->value[0] != '\0')
+			printf("%s=%s\n", current->key, current->value);
 		current = current->next;
 	}
 	return (0);
