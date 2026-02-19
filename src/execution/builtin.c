9ee6fa71f9bdc31de12ec2ceb90ce1820cac263c
Mael Barrere--Musset <mabarrer@3D3.42angouleme.fr>
2026-02-19 21:33:01 +0100

    norm

--- src/execution/builtin.c
+++ src/execution/builtin.c
@@ -6,7 +6,7 @@
 /*   By: fgargot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 18:40:23 by fgargot           #+#    #+#             */
-/*   Updated: 2026/02/18 19:40:07 by fgargot          ###   ########.fr       */
+/*   Updated: 2026/02/19 21:32:17 by mabarrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,45 @@
 #include "string.h"
 #include <unistd.h>
 
-int	is_builtin(t_cmd *cmd)
+int	get_builtin_i(const char *cmd)
 {
+	const char	*cmds[] = {"echo", "cd", "pwd", "export",
+		"unset", "env", "exit"};
 	int			i;
-	const char	*c = cmd->args[0];
-	const char	*cmds[] = {"echo", "cd", "pwd", "export", "unset", "env",
-			"exit"};
 
 	i = 0;
-	if (!c)
-		return (0);
 	while (i < 7)
 	{
-		if (strcmp(c, cmds[i++]) == 0)
-			return (1);
+		if (strcmp(cmd, cmds[i]) == 0)
+			return (i);
+		i++;
 	}
-	return (0);
+	return (-1);
+}
+
+int	is_builtin(t_cmd *cmd)
+{
+	if (!cmd->args[0])
+		return (0);
+	return (get_builtin_i(cmd->args[0]) >= 0);
 }
 
 int	call_builtin(t_node *node, t_list **envs, t_ctx *ctx)
 {
-	char			*c;
-	int				i;
-	int				err_code;
-	int				stdio_fd[2];
-	const char		*cmds[] = {"echo", "cd", "pwd", "export", "unset", "env",
-				"exit"};
-	t_builtin_func	cmds_func[] = {builtin_echo, builtin_cd, builtin_pwd,
-			builtin_export, builtin_unset, builtin_env, builtin_exit};
+	const t_builtin_func	cmds_func[] = {builtin_echo, builtin_cd,
+		builtin_pwd, builtin_export, builtin_unset, builtin_env, builtin_exit};
+	int						stdio_fd[2];
+	int						err_code;
+	int						i;
 
-	c = node->cmd->args[0];
-	i = 0;
+	i = get_builtin_i(node->cmd->args[0]);
 	err_code = 0;
 	stdio_fd[0] = dup(0);
 	stdio_fd[1] = dup(1);
 	dup2(node->fd_out, STDOUT_FILENO);
 	dup2(node->fd_in, STDIN_FILENO);
-	while (i < 7)
-	{
-		if (strcmp(c, cmds[i]) == 0)
-		{
-			err_code = cmds_func[i](node->cmd, envs, ctx);
-			break ;
-		}
-		i++;
-	}
+	if (i >= 0)
+		err_code = cmds_func[i](node->cmd, envs, ctx);
 	dup2(stdio_fd[0], STDIN_FILENO);
 	dup2(stdio_fd[1], STDOUT_FILENO);
 	close(stdio_fd[0]);
