9570aac8ac319beabcb616dbe32fe2d21d59cace
Mael Barrere--Musset <mabarrer@3D3.42angouleme.fr>
2026-02-19 22:48:53 +0100

    normed

--- src/parser/redir.c
+++ src/parser/redir.c
@@ -6,7 +6,7 @@
 /*   By: fgargot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 14:41:01 by fgargot           #+#    #+#             */
-/*   Updated: 2026/02/19 22:15:05 by fgargot          ###   ########.fr       */
+/*   Updated: 2026/02/19 22:48:12 by mabarrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,32 +37,42 @@ int	is_redirection(t_token_type type)
 		|| type == TOKEN_APPEND || type == TOKEN_HEREDOC);
 }
 
-void	add_redirection(t_cmd *cmd, t_token **tokens)
+static t_redir	*new_redir(t_token **tokens)
 {
 	t_redir	*redir;
-	t_redir	*tmp;
 
-	redir = (t_redir *)malloc(sizeof(t_redir));
+	redir = malloc(sizeof(t_redir));
 	if (!redir)
-		return ;
+		return (NULL);
 	redir->type = (*tokens)->type;
+	redir->next = NULL;
 	*tokens = (*tokens)->next;
 	if (!*tokens || (*tokens)->type != TOKEN_WORD)
 	{
 		fprintf(stderr, "error redir not filename\n");
 		free(redir);
-		return ;
+		return (NULL);
 	}
 	redir->file = ft_strdup((*tokens)->value);
 	*tokens = (*tokens)->next;
-	redir->next = NULL;
+	return (redir);
+}
+
+void	add_redirection(t_cmd *cmd, t_token **tokens)
+{
+	t_redir	*redir;
+	t_redir	*tmp;
+
+	redir = new_redir(tokens);
+	if (!redir)
+		return ;
 	if (!cmd->redirs)
-		cmd->redirs = redir;
-	else
 	{
-		tmp = cmd->redirs;
-		while (tmp->next)
-			tmp = tmp->next;
-		tmp->next = redir;
+		cmd->redirs = redir;
+		return ;
 	}
+	tmp = cmd->redirs;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = redir;
 }
